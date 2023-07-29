22304475305f42653c1cff6d08777356ab4083a7
Oleg Pentsov <OlegPensov@gmail.com>
2023-07-29 21:49:11 +0300

    Initial commit

---
+++ client/desktop/src/com/silauras/client/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.silauras.client;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.silauras.client.MyGdxGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("SimpleMultiplayerRPG");
+		new Lwjgl3Application(new MyGdxGame(), config);
+	}
+}
