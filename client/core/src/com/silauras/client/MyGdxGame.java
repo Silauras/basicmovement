16a98f514d1d0b636d2f5d8087f4a5b64b5e1982
Oleg Pentsov <OlegPensov@gmail.com>
2023-07-29 22:20:19 +0300

    Added textures and simple printing floor

--- client/core/src/com/silauras/client/MyGdxGame.java
+++ client/core/src/com/silauras/client/MyGdxGame.java
@@ -1,31 +1,36 @@
 package com.silauras.client;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture floorTexture;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		floorTexture = new Texture("basic_floor_tile.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		for (int i = 0; i < Gdx.graphics.getWidth(); i+= floorTexture.getWidth()) {
+			for (int j = 0; j < Gdx.graphics.getHeight(); j+= floorTexture.getHeight()) {
+				batch.draw(floorTexture, i, j);
+			}
+		}
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		floorTexture.dispose();
 	}
 }
